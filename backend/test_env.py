36d9565a8241f2df84649426bcd7259f69695b23
Ali Abdullah <ali.abdulllah.003@gmail.com>
2026-06-27 22:46:59 +0500

    chore: remove unused speech_demo and test_env scripts

--- backend/test_env.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("AZURE_SPEECH_KEY:", os.getenv("AZURE_SPEECH_KEY"))
-print("AZURE_SPEECH_REGION:", os.getenv("AZURE_SPEECH_REGION"))
-print("AZURE_LANGUAGE_KEY:", os.getenv("AZURE_LANGUAGE_KEY"))
-print("AZURE_LANGUAGE_ENDPOINT:", os.getenv("AZURE_LANGUAGE_ENDPOINT"))
